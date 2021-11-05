e2f780ec99611da5045510d8206de90d036a186f
Mariana Theml <marianatheml@gmail.com>
2021-11-05 12:42:34 -0300

    @marianatheml [task: CREATE Sprint Boot Project]

---
+++ myGameStore/src/test/java/com/generation/myGameStore/MyGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.myGameStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
