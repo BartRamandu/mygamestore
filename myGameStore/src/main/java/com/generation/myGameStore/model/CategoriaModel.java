8e4cd29b54fd9e3d4d0993a75959a908bd2321dc
pedrosantosara <pedrohensantosaraujo@gmail.com>
2021-11-05 18:54:14 -0300

    @pedrosantosara [Task - CREATE ProdutoModel and Foreigns Keys]

---
+++ myGameStore/src/main/java/com/generation/myGameStore/model/CategoriaModel.java
@@ -0,0 +1,87 @@
+package com.generation.myGameStore.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author BabiLuna
+ * @author pedro
+ * @since 1.0
+ */
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String categoria;
+
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String descricao;
+
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String formaPagamento;
+
+	@OneToMany(mappedBy = "categoriaDoProduto", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoriaDoProduto")
+	private List<ProdutoModel> produtos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public List<ProdutoModel> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<ProdutoModel> produtos) {
+		this.produtos = produtos;
+	}
+
+}
---
+++ myGameStore/src/main/java/com/generation/myGameStore/model/ProdutoModel.java
@@ -0,0 +1,98 @@
+package com.generation.myGameStore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * 
+ * @author Pedro
+ * @version 1.0
+ *
+ */
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String produto;
+
+	@NotBlank
+	@Size(min = 2, max = 500)
+	private String descricao;
+
+	@NotNull
+	private int estoque;
+
+	@NotNull
+	private double valor;
+
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_categoria")
+	@JsonIgnoreProperties("produtos")
+	private CategoriaModel categoriaDoProduto;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public CategoriaModel getCategoriaDoProduto() {
+		return categoriaDoProduto;
+	}
+
+	public void setCategoriaDoProduto(CategoriaModel categoriaDoProduto) {
+		this.categoriaDoProduto = categoriaDoProduto;
+	}
+
+}
