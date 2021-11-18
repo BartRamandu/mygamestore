b497ac6828da23b4eef1947c3eacf42d9b3cb87f
Mariana Theml <marianatheml@gmail.com>
2021-11-18 15:40:55 -0300

    @marianatheml [Task:  Deploy Application]

--- myGameStore/src/main/java/com/generation/myGameStore/security/BasicSecurityConfig.java
+++ myGameStore/src/main/java/com/generation/myGameStore/security/BasicSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.generation.myGameStore.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,6 +36,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
